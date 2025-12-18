c75f877f2b2b4c26ac7e880148ca239da070b2e5
Hiroki_ujita <hiroki_ujita@hotmail.com>
2025-12-18 13:30:08 +0900

    問題16

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import Practice07 from "./Practice07";
 import Practice08 from "./Practice08";
 import Practice12 from "./Practice12";
 import Practice15 from "./Practice15";
+import Practice16 from "./Practice16";
 
 function App() {
   return (
     <div>
       <h1>Web API 練習</h1>
-      <Practice15 />
+      <Practice16 />
     </div>
   );
 }
